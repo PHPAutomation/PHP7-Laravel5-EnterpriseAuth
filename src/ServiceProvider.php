d2c578f7c1cd4f12fb31d2ed23d06698daeefdd6
root <root@fakemail.com>
2018-02-23 12:39:32 -0600

    fixed more bugs

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -77,10 +77,10 @@ class ServiceProvider extends BaseServiceProvider
         });
         // This handles a situation where a route with the NAME of login does not exist, we define it to keep from breaking framework redirects hard coded
         if (! \Route::has('login') ) {
-            $this->app['router']->get('login', Metrogistics\AzureSocialite\AuthController@loginOrRegister)->name('login');
+            $this->app['router']->get('login', 'Metrogistics\AzureSocialite\AuthController@loginOrRegister')->name('login');
         }
         if (! \Route::has('register') ) {
-            $this->app['router']->get('register', Metrogistics\AzureSocialite\AuthController@loginOrRegister)->name('register');
+            $this->app['router']->get('register', 'Metrogistics\AzureSocialite\AuthController@loginOrRegister')->name('register');
         }
     }
 }
