c4ff34ec8b5bf5dd227e9aec0a96e3ecf8b4ae6e
metaclassing <metaclassing@secureobscure.com>
2018-05-13 21:30:45 -0500

    simplified the boot function for the service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace Metaclassing\EnterpriseAuth;
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\ServiceProvider as BaseServiceProvider;
-
-class ServiceProvider extends BaseServiceProvider
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
     {
@@ -14,32 +11,13 @@ class ServiceProvider extends BaseServiceProvider
     public function boot()
     {
         // Make sure nobody is including or running this thing without all the required env settings
-        if (config('enterpriseauth')) {
-            foreach (config('enterpriseauth.credentials') as $config => $env) {
-                if (! config('enterpriseauth.credentials.'.$config)) {
-                    throw new \Exception('enterpriseauth setup error: missing mandatory config value for enterpriseauth.credentials.'.$config.' check your .env file!');
-                }
-            }
-        }
+        $this->checkMandatoryConfigsAreSet();
 
-        // Actually I have my own oauth token cache based authentication guard now lol
-        config(['auth.guards.api.driver' => 'oauthtoken']);
-        Auth::extend('oauthtoken', function ($app, $name, array $config) {
-            return new \Metaclassing\EnterpriseAuth\Middleware\OauthTokenGuard(Auth::createUserProvider($config['provider']), $app->make('request'));
-        });
+        // Install our API auth guard middleware
+        $this->installOauthTokenGuardMiddleware();
 
         // Make sure that this vendor dir and the routes dir are in any scanned paths for swagger documentation
-        $swaggerScanPaths = config('l5-swagger.paths.annotations');
-        if (! is_array($swaggerScanPaths)) {
-            $swaggerScanPaths = [$swaggerScanPaths];
-        }
-        if (! in_array(base_path('routes'), $swaggerScanPaths)) {
-            $swaggerScanPaths[] = base_path('routes');
-        }
-        if (! in_array(__DIR__.'/../routes/', $swaggerScanPaths)) {
-            $swaggerScanPaths[] = __DIR__.'/../routes/';
-        }
-        config(['l5-swagger.paths.annotations' => $swaggerScanPaths]);
+        $this->configureSwaggerToScanEnterpriseAuthRouteFiles();
 
         // Make sure the publish command picks up our config, migration, user model, and dummy API route files
         $this->publishes([
@@ -60,6 +38,46 @@ class ServiceProvider extends BaseServiceProvider
 
         // Trigger generating our swagger oauth security settings based on application env file contents
         $this->generateSwaggerOauthSecurityScheme();
+    }
+
+    protected function checkMandatoryConfigsAreSet()
+    {
+        // On first run this will be false, after config file is installed it will be true
+        if (config('enterpriseauth')) {
+            // Go through all the credential config and make sure they are set in the .env or config file
+            foreach (config('enterpriseauth.credentials') as $config => $env) {
+                // If one isnt set, throw a red flat until the person fixes it
+                if (! config('enterpriseauth.credentials.'.$config)) {
+                    throw new \Exception('enterpriseauth setup error: missing mandatory config value for enterpriseauth.credentials.'.$config.' check your .env file!');
+                }
+            }
+        }
+    }
+
+    protected function installOauthTokenGuardMiddleware()
+    {
+        // Override the application configuration to use our oauth token guard driver at runtime
+        config(['auth.guards.api.driver' => 'oauthtoken']);
+        // Now I have a machine gun. ho ho ho!
+        \Illuminate\Support\Facades\Auth::extend('oauthtoken', function ($app, $name, array $config) {
+            $userProvider = \Illuminate\Support\Facades\Auth::createUserProvider($config['provider']);
+            return new \Metaclassing\EnterpriseAuth\Middleware\OauthTokenGuard($userProvider, $app->make('request'));
+        });
+    }
+
+    protected function configureSwaggerToScanEnterpriseAuthRouteFiles()
+    {
+        $swaggerScanPaths = config('l5-swagger.paths.annotations');
+        if (! is_array($swaggerScanPaths)) {
+            $swaggerScanPaths = [$swaggerScanPaths];
+        }
+        if (! in_array(base_path('routes'), $swaggerScanPaths)) {
+            $swaggerScanPaths[] = base_path('routes');
+        }
+        if (! in_array(__DIR__.'/../routes/', $swaggerScanPaths)) {
+            $swaggerScanPaths[] = __DIR__.'/../routes/';
+        }
+        config(['l5-swagger.paths.annotations' => $swaggerScanPaths]);
     }
 
     protected function generateSwaggerOauthSecurityScheme()
