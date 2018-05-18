26cce768278b0c20eb9ff3be3bd9b1fef1a84267
metaclassing <metaclassing@secureobscure.com>
2018-05-18 16:40:27 -0500

    improvements

--- src/Controllers/ApiAuthController.php
+++ src/Controllers/ApiAuthController.php
@@ -101,7 +101,7 @@ class ApiAuthController extends AuthController
                 'id'                => $appData->azp,
                 'displayName'       => $appData->azp,
                 'mail'              => $appData->azp,
-                'userPrincipalName' => $appData->azp,
+                'userPrincipalName' => '',
             ];
 
         // This is a laravel \App\User
