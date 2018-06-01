3e74691f78ff545ef54b1ad04536b75892e612e3
metaclassing <metaclassing@secureobscure.com>
2018-06-01 07:22:48 -0500

    i dont think i need these turds anymore do i

--- src/Controllers/ApiAuthController.php
+++ src/Controllers/ApiAuthController.php
@@ -3,7 +3,6 @@
 namespace Metaclassing\EnterpriseAuth\Controllers;
 
 use Illuminate\Routing\Controller;
-use Laravel\Socialite\Facades\Socialite;
 
 class ApiAuthController extends AuthController
 {
